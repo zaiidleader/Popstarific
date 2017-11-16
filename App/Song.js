34dbe14ba387157363da093d1f7306a87dd9fcd1
zaiidleader <dleader.zaii@gmail.com>
2017-11-16 15:39:36 +0700

    Update Song.js

--- App/Song.js
+++ App/Song.js
@@ -8,32 +8,33 @@ import {
 
 export default class Song extends Component {
 
-	onPlay() {
-		this.props.playSong(this.props.item.path)
-	}
+  onPlay() {
+    this.props.playSong(this.props.item.path)
+    this.props.changeTitle(this.props.item.title)
+  }
 
-	render () {
+  render () {
 
-		let containerStyle = (this.props.item.isPlaying) ? styles.playing : styles.notPlaying
+    let containerStyle = (this.props.item.isPlaying) ? styles.playing : styles.notPlaying
 
-		return (
-			<TouchableOpacity onPress={this.onPlay.bind(this)} activeOpacity={ 0.5 } style={styles.container}>
-				<View style={containerStyle}>
-					<View>
-						<Text style={styles.title}>
-							{this.props.item.author}
-						</Text>
-					</View>
+    return (
+      <TouchableOpacity onPress={this.onPlay.bind(this)} activeOpacity={ 0.5 } style={styles.container}>
+        <View style={containerStyle}>
+          <View>
+            <Text style={styles.title}>
+              {this.props.item.author}
+            </Text>
+          </View>
 
-					<View>
-						<Text style={styles.author}>
-							{this.props.item.title}
-						</Text>
-					</View>
-				</View>
-			</TouchableOpacity>
-		)
-	}
+          <View>
+            <Text style={styles.author}>
+              {this.props.item.title}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -41,20 +42,20 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   notPlaying: {
-		backgroundColor: '#FFFFFF'
-	},
-	playing: {
-		backgroundColor: '#EEEFF1'
-	},
+    backgroundColor: '#FFFFFF'
+  },
+  playing: {
+    backgroundColor: '#EEEFF1'
+  },
   title: {
     textAlign: 'left',
     margin: 10,
-	},
+  },
   author: {
-		fontSize: 16,
+    fontSize: 16,
     textAlign: 'left',
     color: '#333333',
     marginLeft: 10,
     marginBottom: 10,
-	},
+  },
 })
